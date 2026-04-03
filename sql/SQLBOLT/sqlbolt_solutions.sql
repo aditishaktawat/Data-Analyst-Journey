848effaeb3b3ba78bb74a276b5fe1e78f42780e6
aditishaktawat <aditishaktawat4@gmail.com>
2026-04-03 12:12:07 +0530

    docs: complete SQLBolt Lesson 2 on WHERE constraints

--- sql/SQLBOLT/sqlbolt_solutions.sql
+++ sql/SQLBOLT/sqlbolt_solutions.sql
@@ -12,3 +12,12 @@ SELECT title,year FROM movies;
 -- 5. Find all the information about each film
 SELECT * FROM movies;
 
+-- SQL Lesson 2: Queries with constraints (Pt. 1)
+-- 1.Movie with a row id of 6 
+SELECT title FROM movies where id=6;
+-- 2. Movies released in the years between 2000 and 2010
+SELECT title FROM movies where year between 2000 and 2010;
+-- 3. Movies not released in the years between 2000 and 2010
+SELECT title FROM movies where year not between 2000 and 2010;
+-- 4. First 5 Pixar movies and their release year
+SELECT title,year FROM movies where id BETWEEN 1 AND 5;
