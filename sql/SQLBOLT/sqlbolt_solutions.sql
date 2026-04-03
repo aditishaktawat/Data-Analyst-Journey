9c553d08fce315964fd124736c0cca871c27947d
aditishaktawat <aditishaktawat4@gmail.com>
2026-04-03 11:49:45 +0530

    docs: complete SQLBolt Lesson 1 solutions

---
+++ sql/SQLBOLT/sqlbolt_solutions.sql
@@ -0,0 +1,14 @@
+/* LESSON 1: SELECT Queries 101
+Focus: Understanding the basic anatomy of a SELECT statement.
+*/
+-- 1. Get film titles
+SELECT Title FROM movies;
+-- 2. Find directors
+SELECT director FROM movies;
+-- 3. Find title and directors
+SELECT title,director FROM movies;
+-- 4. Find title and year
+SELECT title,year FROM movies;
+-- 5. Find all the information about each film
+SELECT * FROM movies;
+
