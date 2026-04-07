dfdf023360f2cc4c99c4beff0ba1140edacbc213
aditishaktawat <aditishaktawat4@gmail.com>
2026-04-07 17:44:37 +0530

    feat(sql): Implement data changes in databases

--- sql/SQLBOLT/sqlbolt_solutions.sql
+++ sql/SQLBOLT/sqlbolt_solutions.sql
@@ -183,4 +183,28 @@ INSERT INTO BoxOffice
  VALUES (15,8.7,340,270)
 
 
+-- SQL Lesson 14: Updating rows
+-- UPDATE mytable
+-- SET column = value_or_expr, 
+--     other_column = another_value_or_expr, 
+--     …
+-- WHERE condition;
+
+-- 1.The director for A Bug's Life is incorrect, it was actually directed by John Lasseter ✓
+UPDATE movies
+ SET Director = "John Lasseter"
+  WHERE id = 2;
+-- 2.The year that Toy Story 2 was released is incorrect, it was actually released in 1999 ✓
+UPDATE movies
+ SET year = 1999
+  WHERE id = 3;
+
+-- 3.Both the title and director for Toy Story 8 is incorrect! The title should be "Toy Story 3" and it was directed by Lee Unkrich ✓
+UPDATE movies
+ SET title = "Toy Story 3",
+     Director = "Lee Unkrich"
+  WHERE id = 11;
+
+  
+
 
