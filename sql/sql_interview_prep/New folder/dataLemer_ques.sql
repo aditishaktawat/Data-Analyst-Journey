38797dd9c598d20a9acb1d69afb1910235c9b7e1
aditishaktawat <aditishaktawat4@gmail.com>
2026-04-14 12:43:39 +0530

    (sql): Implemnt Tesla Unfinised Parts Problem

--- sql/sql_interview_prep/New folder/dataLemer_ques.sql
+++ sql/sql_interview_prep/New folder/dataLemer_ques.sql
@@ -12,8 +12,8 @@ HAVING COUNT(skill) = 3
 ORDER BY candidate_id;
 
 -- QUESTION: Page With No Likes [Facebook]
--- STATUS: DONE
-
+-- STATUS: DONE  
+-- 13 min
 SELECT p.page_id
 FROM pages as p 
 LEFT JOIN page_likes as pl 
@@ -21,6 +21,31 @@ LEFT JOIN page_likes as pl
 GROUP BY p.page_id HAVING count(user_id) = 0
 ORDER BY p.page_id ASC;
 
+-- Alternative with faster processing
+SELECT p.page_id
+FROM pages as p 
+LEFT JOIN page_likes as pl    --left join will automatically eliminate the pages with likes
+  ON p.page_id = pl.page_id
+WHERE pl.page_id IS NULL
+ORDER BY p.page_id ASC;
+
 /* INSIGHTS: 
 This helps to identify the dead pages and eliminate them.
 */
+
+-- QUESTION: Unfinished Parts [Tesla]
+-- STATUS: DONE  
+-- 3 min
+
+SELECT part, assembly_step
+FROM parts_assembly
+WHERE finish_date IS NULL;
+
+/* BUISNESS INSIGHTS:
+It helps in identifying the parts which are lagging behind and need a special attention to find if there's an internal issue.
+
+
+-- QUESTION: Laptop vs. Mobile Viewership [NY TIMES]
+-- STATUS: DONE  
+-- 3 min
+
