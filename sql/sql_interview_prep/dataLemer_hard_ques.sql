f83afd111e90daf74e19efc2311afd65421dcf72
aditishaktawat <aditishaktawat4@gmail.com>
2026-06-21 17:00:40 +0530

    feat(sql):add Wayfair y-o-y growth rate soultion and insight

--- sql/sql_interview_prep/dataLemer_hard_ques.sql
+++ sql/sql_interview_prep/dataLemer_hard_ques.sql
@@ -19,3 +19,35 @@ ORDER BY user_id ;
 Categorizes users into distinct lifecycle stages to track user base health and revenue stability.
 This allows growth and marketing teams to identify if revenue changes are driven by acquiring new users, retaining existing ones, losing customers, or successfully winning back old ones.
 */
+
+
+--QUESTION: Y-on-Y Growth Rate [Wayfair]
+--STATUS: Done 
+--TIME : 10 min
+
+WITH yearly_sales AS (
+  SELECT 
+    EXTRACT(YEAR FROM transaction_date) as year,
+    product_id,
+    spend as curr_year_spend,
+    LAG(spend) OVER( PARTITION by product_id ORDER BY EXTRACT(YEAR FROM transaction_date))
+      as prev_year_spend
+  FROM user_transactions
+)
+SELECT year,
+       product_id,
+       curr_year_spend,
+       prev_year_spend,
+       Round((curr_year_spend - prev_year_spend)/prev_year_spend*100,2) as as yoy_rate
+FROM yearly_sales
+
+/*BUISNESS INSIGHTS-
+Tracks Year-over-Year (YoY) revenue growth at the individual product level.
+This helps leadership quickly identify which products are gaining market traction and which are shrinking,
+ allowing them to shift marketing budgets toward winning items.*/
+
+
+
+--QUESTION:  [Wayfair]
+--STATUS: Done 
+--TIME :
